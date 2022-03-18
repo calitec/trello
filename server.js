144b9527cd41ed8bb53c6c29b86b9e09be5d0646
calininja <syg0713@gmail.com>
2022-03-18 22:04:39 +0900

    test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const compression = require("compression");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.disable("x-powered-by");
+app.use(compression());
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`express running on port:${port}`);
+});
